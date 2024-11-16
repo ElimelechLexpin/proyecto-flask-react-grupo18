f1df4a8b01639c84323e9f223244d3511eca11cb
Elimelech <elimelechattale@lexpin.online>
2024-11-15 20:40:27 -0400

    Create App.jsx

---
+++ proyecto-flask-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import CreateUser from '';
+import RemoveUser from '';
+import UpdateUser from '';
+
+function App() {
+   return ( 
+     <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/craete-user" element={<CreateUser />} />
+        <Route path="/remove-user" element={<RemoveUser />} />
+        <Route path="/update-user" element={<UpdateUser />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
